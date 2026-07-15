7359e394d9485da196d38d6f9e6edb251568617c
Pedro Guimaraes <pedrosbarcelos98@gmail.com>
2026-07-14 21:53:53 -0300

    feat: criados os componentes da drawer e do botão de carrinho

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,63 +1,69 @@
-import Logo from '@/assets/images/logo.png'
-import IconUser from '@/assets/images/icon-user.png'
-import { Link } from '@tanstack/react-router'
-import { ShoppingCart } from '../ShoppingCart'
-import { MenuMobile } from '../MenuMobile'
+import Logo from "@/assets/images/logo.png";
+import IconUser from "@/assets/images/icon-user.png";
+import { Link } from "@tanstack/react-router";
+import { MenuMobile } from "../MenuMobile";
+import { CartButton } from "../CartButton";
+import { CartDrawer } from "../CartDrawer";
+import { useState } from "react";
 
 export interface NavLink {
-    name: string;
-    href: string;
+  name: string;
+  href: string;
 }
 
 const navLinks: NavLink[] = [
-    { name: "Masculino", href: "/products/category/masculino" },
-    { name: "Feminino", href: "/products/category/feminino" },
-    { name: "Outlet", href: "/products/category/outlet" },
-]
+  { name: "Masculino", href: "/products/category/masculino" },
+  { name: "Feminino", href: "/products/category/feminino" },
+  { name: "Outlet", href: "/products/category/outlet" },
+];
 
 export const Header = () => {
-    return (
-        <div className="relative">
-            <header className="fixed top-5 left-0 right-0 z-10 mx-10">
-                <div className="bg-white text-black max-w-330 mx-auto flex justify-between items-center py-3 px-7 rounded-2xl mt-5">
 
-                    <Link to="/">
-                        <img src={Logo} alt="Logo" className="w-32 md:w-36" />
-                    </Link>
+  const [cartIsOpen, setCartIsOpen] = useState<boolean>(false);
 
-                    <nav className="hidden lg:block">
-                        <ul className="flex gap-10">
-                            {navLinks.map((link) => (
-                                <Link to={link.href} key={link.name}>
-                                    {link.name}
-                                </Link>
-                            ))}
-                        </ul>
-                    </nav>
+  return (
+    <div className="relative">
+      <header className="fixed top-5 left-0 right-0 z-10 mx-10">
+        <div className="bg-white text-black max-w-[1320px] mx-auto flex justify-between items-center py-3 px-7 rounded-2xl mt-5">
+          <Link to="/">
+            <img src={Logo} alt="Logo SyntaxWear" className="w-32 md:w-36" />
+          </Link>
 
-                    <nav>
-                        <ul className="flex gap-4 md:gap-10 items-center">
-                            <li className="hidden lg:block">
-                                <Link to="/our-stores">Nossas lojas</Link>
-                            </li>
-                            <li className="hidden lg:block">
-                                <Link to="/about">Sobre</Link>
-                            </li>
-                            <li className='lg:hidden'>
-                                <MenuMobile navLinks={navLinks} />
-                            </li>
-                            <li className='hidden lg:block'>
-                                <Link to="/sign-up">
-                                    <img src={IconUser} alt="Ícone de login" />
-                                </Link>
-                            </li>
-                            <li>
-                                <ShoppingCart />
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
-            </header>
+          <nav className="hidden lg:block">
+            <ul className="flex gap-10">
+              {navLinks.map((link) => (
+                <Link to={link.href} key={link.name}>
+                  {link.name}
+                </Link>
+              ))}
+            </ul>
+          </nav>
+
+          <nav>
+            <ul className="flex gap-4 md:gap-10 items-center">
+              <li className="hidden lg:block">
+                <Link to="/our-stores">Nossas lojas</Link>
+              </li>
+              <li className="hidden lg:block">
+                <Link to="/about">Sobre</Link>
+              </li>
+              <li className="lg:hidden">
+                <MenuMobile navLinks={navLinks} />
+              </li>
+              <li className="hidden lg:block">
+                <Link to="/sign-up">
+                  <img src={IconUser} alt="Ícone de login" />
+                </Link>
+              </li>
+              <li>
+                <CartButton onClick={() => setCartIsOpen(true)}/>
+              </li>
+            </ul>
+          </nav>
         </div>
-    )
-}
+      </header>
+
+      <CartDrawer isOpen={cartIsOpen} onClose={() => setCartIsOpen(false)} />
+    </div>
+  );
+};
